284613a96bac6c74ffd167dae87f322efd05705b
saarthamin <samin9@student.gsu.edu>
2017-04-25 20:21:28 -0400

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -1,45 +1,59 @@
 package CSKAirlines;
 
-public class Reservation {
+public class Reservation implements Persistable{
 	protected User customer;
 	protected Flight flight;
-	protected long reservationNumber;  // Unique reservation number across the system
+	protected boolean _isDirty = false;//see if database needs to be updated - set to true when you are changing something - setter methods
+	protected int reservationNumber; //Unique reservation number across the system - incremented in database 
 	
-	public Reservation(long reservationNumber, User customer, Flight flight){
+	public Reservation(int reservationNumber, User customer, Flight flight){
 		this.reservationNumber = reservationNumber;
 		this.customer = customer;
 		this.flight = flight;
 	}
 	
+	public boolean isDirty() {
+		return _isDirty;
+	}
+
+	public void save() {//after changes are made by the User have to call this method to update database
+		if(_isDirty)
+			DB.getInstance().update(this);
+		_isDirty = false;
+	}
 	
 	public User getCustomer() {
 		return customer;
 	}
 	public void setCustomer(User customer) {
 		this.customer = customer;
+		_isDirty = true;
 	}
 	public Flight getFlight() {
 		return flight;
 	}
 	public void setFlight(Flight flight) {
 		this.flight = flight;
+		_isDirty = true;
 	}
 
-	public long getReservationNumber() {
+	public int getReservationNumber() {
 		return reservationNumber;
 	}
-	public void setReservationNumber(long reservationNumber) {
+	public void setReservationNumber(int reservationNumber) {
 		this.reservationNumber = reservationNumber;
+		_isDirty = true;
 	}
-  
-  //methods to get from DB
+
 	public static Reservation[] getReservationsByCustomer(User customer){
+		return DB.getInstance().getReservationsByCustomer(customer);
 	}
-	
-  public static Reservation getReservationByNumber(long reservationNumber){
+	public static Reservation getReservationByNumber(int reservationNumber){
+		return DB.getInstance().getReservationByNumber(reservationNumber);
 	}
 	
 	public static Reservation[] getReservationsByFlight(Flight flight){
+		return DB.getInstance().getReservationsByFlight(flight);
 	}
     
 	public boolean isEqual(Reservation other){
@@ -50,7 +64,7 @@ public class Reservation {
 	}
 	
 	public String toString(){
-		return "Reservation[ "+reservationNumber + " " 
+		return "Reservation[ "+ this.reservationNumber + " " 
 				+ (this.customer == null ? " Customer[null] " : this.customer)  
 				+ (this.flight == null ? " Flight[null] " : this.flight) +
 				" ]";
