f5223f9528c9701019b96b7cbfcb72ef1ab53e25
saarthamin <samin9@student.gsu.edu>
2017-04-25 22:10:12 -0400

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,3 +1,8 @@
+package CSKAirlines;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
 public class Flight implements Persistable {
 	protected String departureCity;
 	protected String arrivalCity;
@@ -5,8 +10,10 @@ public class Flight implements Persistable {
 	protected Date arrivalTime;    // local date time
 	protected int flightNumber;
 	
-	protected int capacity; // if we have more than one type of flight
-	
+	protected int capacity;
+	ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+
+	protected boolean _isDirty = false;
 	
 	public Flight(String departureCity, String arrivalCity, Date departureTime,
 			Date arrivalTime, int flightNumber, int capacity){
@@ -17,16 +24,43 @@ public class Flight implements Persistable {
 		this.flightNumber = flightNumber;
 		this.capacity = capacity;
 	}
+	public boolean isDirty() {
+		return _isDirty;
+	}
+
+	public void save() {//after updates are made - you must call this method to save to database 
+		if(_isDirty)
+			DB.getInstance().update(this);
+		_isDirty = false;
+	}
 
 	/*
-	** Need to write search flights methods
+	** search flights
 	*/
 	
+	public static Flight[] getAllFlights() {
+		return DB.getInstance().getAllFlights();
+	}
+	public static Flight getFlightByDepartureCityAndDateAndNumber(String departureCity, Date departureDate, int flightNumber){
+		return DB.getInstance().getFlightByDepartureCityAndDateAndNumber(departureCity, departureDate, flightNumber);
+	}
+	
+	public static Flight[] getFlightByNumberAndDepartureDate(int flightNumber, Date departureDate){
+		return DB.getInstance().getFlightByNumberAndDepartureDate(flightNumber, departureDate);
+	}
+	
+	public static Flight[] getFlightByDepartureCityAndDate(String departureCity, Date departureDate){
+		return DB.getInstance().getFlightByDepartureCityAndDate(departureCity, departureDate);
+	}
+	
+	public static Flight[] getFlightByDepartureCityAndArrivalCity(String departureCity, String arrivalCity){
+		return DB.getInstance().getFlightByDepartureCityAndArrivalCity(departureCity, arrivalCity);
+	}
 
-	public boolean isEqual(Flight other) {
+	public boolean equals(Flight other) {
 		if(
-			this.departureCity == other.departureCity &&
-			this.arrivalCity == other.arrivalCity &&
+			this.departureCity.equalsIgnoreCase(other.departureCity) &&
+			this.arrivalCity.equalsIgnoreCase(other.arrivalCity) &&
 			this.departureTime.equals(other.departureTime) &&
 			this.arrivalTime.equals(other.arrivalTime) &&
 			this.flightNumber == other.flightNumber &&
@@ -35,46 +69,66 @@ public class Flight implements Persistable {
 			return true;
 		return false;
 	}
+	
+	@Override
+	public String toString() {
+		return "Flight[" + flightNumber + ", " + departureCity + ", " + arrivalCity + ", " + departureTime + ", " + arrivalTime + ", " + capacity + "]";
+	}
 
 	/*
 	** getter and setters
 	*/
-	
+
 	public String getDepartureCity() {
 		return departureCity;
 	}
 	public void setDepartureCity(String departureCity) {
 		this.departureCity = departureCity;
+		_isDirty = true;
 	}
 	public String getArrivalCity() {
 		return arrivalCity;
 	}
 	public void setArrivalCity(String arrivalCity) {
 		this.arrivalCity = arrivalCity;
+		_isDirty = true;
 	}
 	public Date getDepartureTime() {
 		return departureTime;
 	}
 	public void setDepartureTime(Date departureTime) {
 		this.departureTime = departureTime;
+		_isDirty = true;
 	}
 	public Date getArrivalTime() {
 		return arrivalTime;
 	}
 	public void setArrivalTime(Date arrivalTime) {
 		this.arrivalTime = arrivalTime;
+		_isDirty = true;
 	}
 	public int getFlightNumber() {
 		return flightNumber;
 	}
 	public void setFlightNumber(int flightNumber) {
 		this.flightNumber = flightNumber;
+		_isDirty = true;
 	}
 	public int getCapacity() {
 		return capacity;
 	}
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
+		_isDirty = true;
 	}
-	
+	public int getBookingCount() {
+		return DB.getInstance().getBookingCount(this);
+	}
+	public ArrayList<Reservation> getReservations() {
+		return reservations;
+	}
+	public void setReservations(ArrayList<Reservation> reservations) {
+		this.reservations = reservations;
+		_isDirty = true;
+	}
 }
